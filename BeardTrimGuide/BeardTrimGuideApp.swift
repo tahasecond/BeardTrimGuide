6e6149dfe9184cd904cefd96f958d8369e26fde7
Taha <tahasecond@gmail.com>
2024-10-28 17:47:59 -0400

    Initial Commit

---
+++ BeardTrimGuide/BeardTrimGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeardTrimGuideApp.swift
+//  BeardTrimGuide
+//
+//  Created by Taha Ahmad on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BeardTrimGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeardTrimGuide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeardTrimGuide
+//
+//  Created by Taha Ahmad on 10/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
